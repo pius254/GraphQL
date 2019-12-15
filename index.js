7de0bca94d1083a12c1b3de40f747d16b1eddb0c
Pius Muriithi <piusmuriithi1@gmail.com>
2019-12-15 10:53:30 +0300

    Initialize GraphQL

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 import express from 'express';
-
+import graphqHTTP from 'express-graphql';
+import schema from './schema';
 const app = express();
 
 app.get('/', (req, res) => {
     res.send('GraphQL is amazing!');
 });
+
+const root = { hello: () => "Hi, i'm Pius" };
+
+app.use('/graphql', graphqHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true
+}));
 
 app.listen(8080, () => console.log('Running server on port localhost:8080/graphql'));
